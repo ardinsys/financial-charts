423827957967011b041a51d2f383b6c2c2fb24bd
Erik Szabó <szabo.erik@ardinsys.eu>
2024-01-24 10:28:58 +0100

    fix: build resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { resolve } from "node:url";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
