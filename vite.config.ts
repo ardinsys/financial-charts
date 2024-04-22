92610c4de0350018884618d299cd9f6981a02889
Erik Szabó <szabo.erik@ardinsys.eu>
2024-04-22 11:54:19 +0200

    fix: change build target to es2020 in esbuild config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  esbuild: {
+    target: "es2020",
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
